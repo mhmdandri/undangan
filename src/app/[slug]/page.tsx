683bf858bbaf0ca19d5eafc522ccca0dd8c8f577
mhmdandri <mhmdandri03@gmail.com>
2025-11-24 17:52:46 +0700

    change name

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-full h-[100dvh] flex justify-center items-center bg-black">
+    <div className="w-full h-dvh flex justify-center items-center bg-black">
       <div className="flex flex-col justify-center gap-8 items-center">
         <div className="text-4xl lg:text-6xl text-white font-light">{`Who's Watching?`}</div>
         <div className="flex gap-4 lg:gap-8 items-center text-gray-400">
@@ -26,8 +26,14 @@ export default function Home() {
               router.push(`https://weddingof.mohaproject.dev/${params.slug}`);
             }}
           >
-            <Image src={"/avatar.jpg"} alt="profile" width={96} height={96} className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white" />
-            <div className="text-lg lg:text-xl text-center">Cica</div>
+            <Image
+              src={"/avatar.jpg"}
+              alt="profile"
+              width={96}
+              height={96}
+              className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white"
+            />
+            <div className="text-lg lg:text-xl text-center">Jane</div>
           </div>
           <div
             className="space-y-2 group hover:text-white cursor-pointer"
@@ -36,8 +42,14 @@ export default function Home() {
               router.push("/summary");
             }}
           >
-            <Image src={"/profile.jpg"} alt="profile" width={96} height={96} className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white" />
-            <div className="text-lg lg:text-xl text-center">Adnri</div>
+            <Image
+              src={"/profile.jpg"}
+              alt="profile"
+              width={96}
+              height={96}
+              className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white"
+            />
+            <div className="text-lg lg:text-xl text-center">John</div>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,11 @@ export default function Home() {
   useEffect(() => {
     setLoading(false);
     document.title = "Wedding of John Doe & Jane Doe";
+    router.push("/guest");
   }, []);
 
   return (
-    <div className="w-full h-[100dvh] flex justify-center items-center bg-black">
+    <div className="w-full h-dvh flex justify-center items-center bg-black">
       <div className="flex flex-col justify-center gap-8 items-center">
         <div className="text-4xl lg:text-6xl text-white font-light">{`Who's Watching?`}</div>
         <div className="flex gap-4 lg:gap-8 items-center text-gray-400">
@@ -25,8 +26,14 @@ export default function Home() {
               router.push("https://weddingof.mohaproject.dev/guest");
             }}
           >
-            <Image src={"/avatar.jpg"} alt="profile" width={96} height={96} className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white" />
-            <div className="text-lg lg:text-xl text-center">Cica</div>
+            <Image
+              src={"/avatar.jpg"}
+              alt="profile"
+              width={96}
+              height={96}
+              className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white"
+            />
+            <div className="text-lg lg:text-xl text-center">Jane</div>
           </div>
           <div
             className="space-y-2 group hover:text-white cursor-pointer"
@@ -35,8 +42,14 @@ export default function Home() {
               router.push("/summary");
             }}
           >
-            <Image src={"/profile.jpg"} alt="profile" width={96} height={96} className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white" />
-            <div className="text-lg lg:text-xl text-center">Andri</div>
+            <Image
+              src={"/profile.jpg"}
+              alt="profile"
+              width={96}
+              height={96}
+              className="rounded-lg max-h-[200px] max-w-[200px] min-h-24 min-w-[96px] w-[10vw] object-cover group-hover:border group-hover:border-white"
+            />
+            <div className="text-lg lg:text-xl text-center">John</div>
           </div>
         </div>
       </div>
